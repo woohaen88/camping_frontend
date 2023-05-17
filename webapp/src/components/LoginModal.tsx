15c474da0fde4b6c1e8a1f81e2e8b450f2b2a949
woohaen88 <woohaen88@gmail.com>
2023-05-17 04:10:12 +0000

    React Hook Form 사용법 익히기

--- webapp/src/components/LoginModal.tsx
+++ webapp/src/components/LoginModal.tsx
@@ -9,24 +9,39 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Text,
   VStack,
 } from "@chakra-ui/react";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import SocialLogin from "./SocialLogin";
+import { useForm } from "react-hook-form";
 
 interface LoginModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+interface ILoginForm {
+  email: string;
+  password: string;
+}
+
 export default function LoginModal({ onClose, isOpen }: LoginModalProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginForm>();
+  const onSubmit = (data: ILoginForm) => {
+    console.log(data);
+  };
   return (
     <Modal onClose={onClose} isOpen={isOpen}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Log in</ModalHeader>
-        <ModalBody>
-          <VStack>
+        <ModalBody as={"form"} onSubmit={handleSubmit(onSubmit)}>
+          <VStack alignItems={"flex-start"}>
             <InputGroup>
               <InputLeftElement
                 children={
@@ -35,8 +50,17 @@ export default function LoginModal({ onClose, isOpen }: LoginModalProps) {
                   </Box>
                 }
               />
-              <Input variant={"filled"} placeholder="E-mail" />
+              <Input
+                isInvalid={Boolean(errors.email?.message)}
+                {...register("email", { required: "please write a email" })}
+                variant={"filled"}
+                placeholder="E-mail"
+              />
             </InputGroup>
+
+            <Text fontSize={"sm"} color={"red.400"}>
+              {errors.email?.message}
+            </Text>
 
             <InputGroup>
               <InputLeftElement
@@ -46,10 +70,20 @@ export default function LoginModal({ onClose, isOpen }: LoginModalProps) {
                   </Box>
                 }
               />
-              <Input variant={"filled"} placeholder="Password" />
+              <Input
+                {...register("password", {
+                  required: "please write a password",
+                })}
+                variant={"filled"}
+                placeholder="Password"
+                type="password"
+              />
             </InputGroup>
+            <Text fontSize={"sm"} color={"red.400"}>
+              {errors.password?.message}
+            </Text>
           </VStack>
-          <Button mt={4} w={"100%"} colorScheme={"blue"}>
+          <Button type="submit" mt={4} w={"100%"} colorScheme={"blue"}>
             Log in
           </Button>
           <SocialLogin />
